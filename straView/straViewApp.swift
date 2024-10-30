2e499b162e9f0cc83647da54a634f14eb9031f8f
jonaswangelin <jonaswangelin@outlook.com>
2024-10-30 14:36:03 +0100

    Initial Commit

---
+++ straView/straViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  straViewApp.swift
+//  straView
+//
+//  Created by Jonas Wangelin on 30.10.24.
+//
+
+import SwiftUI
+
+@main
+struct straViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ straViewTests/straViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  straViewTests.swift
+//  straViewTests
+//
+//  Created by Jonas Wangelin on 30.10.24.
+//
+
+import Testing
+@testable import straView
+
+struct straViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
