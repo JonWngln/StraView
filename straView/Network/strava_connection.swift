bd6b68d79212455a6a4f573ac9498eda3d958d4b
jonaswangelin <jonaswangelin@outlook.com>
2024-10-31 14:44:04 +0100

    add login view + basic project structure

---
+++ straView/Network/strava_connection.swift
@@ -0,0 +1,7 @@
+//
+//  strava_connection.swift
+//  straView
+//
+//  Created by Jonas Wangelin on 31.10.24.
+//
+
---
+++ straView/Views/signInView.swift
@@ -0,0 +1,38 @@
+//
+//  signInView.swift
+//  straView
+//
+//  Created by Jonas Wangelin on 31.10.24.
+//
+
+import SwiftUI
+
+struct signInView: View {
+    var body: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("app_logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: .infinity, height: 150)
+                    .cornerRadius(10)
+                Spacer()
+                    .frame(height: 200)
+                Button(action: {}) {
+                    Text("Sign in with Strava")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    signInView()
+}
